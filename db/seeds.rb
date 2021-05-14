7441a5b22efbb90207341968c3c836f94464a48a
yatsun <yatsunlam25@gmail.com>
2021-05-14 10:46:46 -0400

    passing it to Angelo

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+
+
+# Dog.create({name:"Trixie Pawtell", age:2, img_url:"https://prettypup.co.uk/wp-content/uploads/2020/05/french-bulldog.jpeg"})
+# Dog.create({name:"McFly", age: 5, img_url:"https://www.loveyourdog.com/wp-content/uploads/2019/08/Pitbull-Terrier-Mixes-900x500.jpg"})
+# Dog.create({name:"Ursula", age: 10, img_url:"https://i.insider.com/5dfe62a2954bda02a25cfd52?width=700"})
+# Dog.create({name:"Mashed Potato", age: 2, img_url:"https://i.pinimg.com/originals/65/8f/89/658f89216f56deae9660ebba09cf5de1.jpg"})
+# Dog.create({name:"Gumdrop", age: 6, img_url:"https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1565637384-51DuxaRPXPL.jpg?crop=1xw:1xh;center,top&resize=480%3A%2A"})
+
+
+# User.create({name:"Dev Patel"})
+# User.create({name:"Jennifer Lopez"})
+# User.create({name:"Janelle Monae"})
+
